d7dc379b15831388524cf9105bdf6e9d82f5e837
Kriwam <kizzbond@gmail.com>
2026-01-27 15:36:23 +0100

    fix addHotel payload keys

--- public/javascripts/addHotel.js
+++ public/javascripts/addHotel.js
@@ -1,27 +1,20 @@
 async function addHotel(url) {
   const name = prompt("Provide the new hotel's name");
-  const location = prompt("Provide the new hotel's location");
+  const hotelLocation = prompt("Provide the new hotel's location");
 
-  if (!name || !location) return;
-
-  await fetch(url, {
+  const response = await fetch(url, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    credentials: "same-origin",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      name: name,
-      location: location,
-    }),
-  })
-    .then((response) => {
-      if (response.ok) {
-        location.reload();
-        return;
-      }
-      return Promise.reject(response);
+      Name: name,
+      Location: hotelLocation
     })
-    .catch(() => {
-      alert("Could not create hotel");
-    });
+  });
+
+  if (response.ok) {
+    location.reload();
+  } else {
+    alert("Could not create hotel");
+  }
 }

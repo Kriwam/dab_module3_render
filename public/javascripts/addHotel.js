a5870ce65a64a93858f8fc19a25a66bdde671b55
Kriwam <kizzbond@gmail.com>
2026-01-27 14:38:35 +0100

    Fix delete hotel auth on Render

--- public/javascripts/addHotel.js
+++ public/javascripts/addHotel.js
@@ -1,24 +1,31 @@
 async function addHotel(url) {
-    let name = prompt("Provide the new hotel's name")
-    let hotelLocation = prompt("Provide the new hotel's location")
-    await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify({
-            Name: name,
-            Location: hotelLocation
-        })
-    }).then((response) => {
-        if (response.ok) {
-            const resData = 'Created a new hotel';
-            location.reload()
-            return Promise.resolve(resData);
-        }
-        return Promise.reject(response);
-    })
-      .catch((response) => {
-        alert(response.statusText);
-      });
+  let name = prompt("Provide the new hotel's name");
+  let hotelLocation = prompt("Provide the new hotel's location");
+
+  if (!name || !hotelLocation) {
+    return;
+  }
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'include', // 🔑 THIS is the important fix
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        Name: name,
+        Location: hotelLocation
+      })
+    });
+
+    if (!response.ok) {
+      throw response;
+    }
+
+    location.reload();
+  } catch (err) {
+    alert('Could not create hotel');
+    console.error(err);
+  }
 }
--- public/javascripts/deleteHotel.js
+++ public/javascripts/deleteHotel.js
@@ -1,22 +1,24 @@
-async function deleteHotel(url, hotelId) {
+async function deleteHotel(url, hotelId) { 
     console.log(url, hotelId);
+
     await fetch(url, {
         method: 'DELETE',
+        credentials: 'include', // ✅ REQUIRED on Render
         headers: {
             'Content-type': 'application/json'
         },
         body: JSON.stringify({
             id: hotelId
         })
-    }).then((response) => {
+    })
+    .then((response) => {
         if (response.ok) {
-            const resData = 'Hotel deleted...';
-            location.reload()
-            return Promise.resolve(resData);
+            location.reload();
+            return;
         }
         return Promise.reject(response);
     })
-      .catch((response) => {
+    .catch((response) => {
         alert(response.statusText);
-      });
+    });
 }
